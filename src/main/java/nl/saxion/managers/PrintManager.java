4dae15e0e730ed6acc6c1feea2ee5701c5d216f8
ivana <558296@student.saxion.nl>
2024-12-10 11:20:36 +0100

    added managers and records

---
+++ src/main/java/nl/saxion/managers/PrintManager.java
@@ -0,0 +1,24 @@
+package nl.saxion.managers;
+
+import nl.saxion.Models.Print;
+
+import java.util.ArrayList;
+
+public class PrintManager {
+    private ArrayList<Print> prints;
+
+    public PrintManager() {
+        this.prints = prints = new ArrayList<Print>();
+    }
+
+    public void addPrintTask(){}
+
+    public Print findPrint(String print){
+        return null;
+    }
+
+    public void addPrint(){}
+
+    public static void readPrintsFromFile(String file){}
+}
+
---
+++ src/main/java/nl/saxion/managers/SpoolManager.java
@@ -0,0 +1,18 @@
+package nl.saxion.managers;
+
+import nl.saxion.Models.Spool;
+
+import java.util.ArrayList;
+
+public class SpoolManager {
+    private ArrayList<Spool> spools;
+
+    public SpoolManager() {
+        this.spools = new ArrayList<>();
+    }
+
+    public void addSpool(Spool spool){}
+    public void gerSpool(){}
+    public static void readSpoolsFromFile(String file){}
+
+}
